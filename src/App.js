32533f39fdbee322bb718e008ac63f08a827b9bb
penguinofwinter <winterbirdwinterbird@gmail.com>
2020-05-28 14:12:47 -0400

    footer and page size adjustment

--- src/App.js
+++ src/App.js
@@ -8,39 +8,42 @@ function App() {
   return (
     <div className="demo-big-content">
     <Layout>
-        <Header className="header" title={<Link className="nav-item" style={{textDecoration:'none',color:'white'}} to="/">BILL LIU</Link>} scroll>
-            <Navigation>
-                <Link className="nav-item" to="/about">ABOUT ME</Link>
-                <Link className="nav-item" to="/resume">RESUME</Link>
-                <Link className="nav-item" to="/portfolio">PORTFOLIO</Link>
-                <Link className="nav-item" to="/contact">CONTACT</Link>
-            </Navigation>
-        </Header>
-        <Drawer title={<Link className="nav-item" style={{textDecoration:'none',color:'black'}} to="/">HOME</Link>}>
-            <Navigation>
-                <Link className="nav-item" to="/about">ABOUT ME</Link>
-                <Link className="nav-item" to="/resume">RESUME</Link>
-                <Link className="nav-item" to="/portfolio">PORTFOLIO</Link>
-                <Link className="nav-item" to="/contact">CONTACT</Link>
-            </Navigation>
-        </Drawer>
-        <Content style={{
-          // background: 'rgb(127, 127, 213)',
-          // background: '-webkit-linear-gradient(to bottom, rgb(91, 195, 230), rgb(134, 168, 231), rgb(127, 127, 213))',
-          // background: 'linear-gradient(to bottom, rgb(91, 195, 230), rgb(134, 168, 231), rgb(127, 127, 213))',
-        }}>
-          <div className="page-content">
-            <ul className="box-area">
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-            </ul>
-            <Main/>
+      <Header className="header" title={<Link className="nav-item" style={{textDecoration:'none',color:'white'}} to="/">BILL LIU</Link>} scroll>
+        <Navigation>
+          <Link className="nav-item" to="/about">ABOUT ME</Link>
+          <Link className="nav-item" to="/resume">RESUME</Link>
+          <Link className="nav-item" to="/portfolio">PORTFOLIO</Link>
+          <Link className="nav-item" to="/contact">CONTACT</Link>
+        </Navigation>
+      </Header>
+      <Drawer title={<Link className="nav-item" style={{textDecoration:'none',color:'black'}} to="/">HOME</Link>}>
+        <Navigation>
+          <Link className="nav-item" to="/about">ABOUT ME</Link>
+          <Link className="nav-item" to="/resume">RESUME</Link>
+          <Link className="nav-item" to="/portfolio">PORTFOLIO</Link>
+          <Link className="nav-item" to="/contact">CONTACT</Link>
+        </Navigation>
+      </Drawer>
+      <Content style={{
+        // background: 'rgb(127, 127, 213)',
+        // background: '-webkit-linear-gradient(to bottom, rgb(91, 195, 230), rgb(134, 168, 231), rgb(127, 127, 213))',
+        // background: 'linear-gradient(to bottom, rgb(91, 195, 230), rgb(134, 168, 231), rgb(127, 127, 213))',
+      }}>
+        <div className="page-content">
+          <ul className="box-area">
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+          <Main/>
+          <div className="footer">
+            <p className="footer-text">Made with ♥ in Waterloo © Bill Siyi Liu</p>
           </div>
-        </Content>
+        </div>
+      </Content>
     </Layout>
 </div>
   );
